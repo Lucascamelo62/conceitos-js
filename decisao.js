19bcd1d7ba67e30a6659c7f39b1bd75528423066
camelo <lucascamelo62@gmail.com>
2024-09-23 20:13:34 -0300

    Aula sobre condicional IF-ELSE

---
+++ decisao.js
@@ -0,0 +1,17 @@
+// IF - ELSE
+
+const nome = 'Eduardo'
+
+if (nome == 'Lucas') {
+    console.log('É igual')
+} else {
+    console.log('Não é igual')
+}
+
+let idade = 30
+
+if (idade > 30) {
+    console.log('É igual')
+} else {
+    console.log('Não é igual')
+}
