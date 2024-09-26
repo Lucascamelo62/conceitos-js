8ec3782ae77913cbbb874aa9e82d24e48f84766e
camelo <lucascamelo62@gmail.com>
2024-09-25 22:17:33 -0300

    Subindo exercicio de função

---
+++ exercicio_funcao.js
@@ -0,0 +1,10 @@
+//Exercicio 3 - Funções
+//Criar uma função que soma 2 numeros e retorna o numero somado, deve ser enviado por parametro os dois numeros
+//Retorno: Numero 1 Numero 2 e Soma
+
+function calculadora(valor1, valor2) {
+    return 'Valor 1: ' + valor1 + '\n' + 'Valor 2: ' + valor2 + '\n' + 'Soma: ' + (valor1 + valor2)
+}
+
+const valores = calculadora(50, 2)
+console.log(valores)
